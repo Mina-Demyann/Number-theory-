7f7858f8a296e915a770dd329e2ec205eb50abc4
Mina-Demyann <151796399+Mina-Demyann@users.noreply.github.com>
2024-06-10 22:23:10 +0300

    Update Meeting.cpp

--- Assuit's-vjudge sheet /Meeting.cpp
+++ Assuit's-vjudge sheet /Meeting.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
-#include <vector>
-#include <cmath>
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+#include "vector"
+#include "algorithm"
 
+#include "numeric"
+#include "limits"
+#include "set"
+#define ull unsigned long long
+#define dd double
+#define ld long double
+#define PQ priority_queue
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define pb push_back
+#define S second
+#define F first
+#define MP make_pair
+#define el "\n"
+#define PI 3.14159265
 using namespace std;
+ll M=1e9+7;
+const int sz=1e5+10;
+const int OO=0x3f3f3f3f;
+#define all(my) my.begin(), my.end()
+#define rall(my) my.rbegin(), my.rend()
 
+
+
+// why did not i use tuple instead ? 
+// Using a struct in this context is generally more readable and maintainable than using a tuple. A struct allows you to give
+// meaningful names to the fields (e.g., x, y, r), which makes the code easier to understand. 
 struct Radiator {
     int x, y, r;
 };
